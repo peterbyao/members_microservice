317b83979b196c7f4cb3f7a5bed40cc83b9ec7de
peterbyao <42585479+peterbyao@users.noreply.github.com>
2023-10-24 02:28:46 -0400

    Add files via upload

---
+++ main.py
@@ -0,0 +1,57 @@
+from fastapi import FastAPI, Response
+import uvicorn
+
+# Tutorials used
+# https://fastapi.tiangolo.com/tutorial/first-steps/
+# https://realpython.com/fastapi-python-web-apis/
+# https://fastapi.tiangolo.com/tutorial/body-multiple-params/
+# https://fastapi.tiangolo.com/tutorial/body-updates/
+# https://fastapi.tiangolo.com/tutorial/body/
+
+app = FastAPI()
+
+
+# Index page
+@app.get('/')
+async def root():
+    return {"message": "Hello World"}
+
+
+# GET members
+@app.get("/members/")
+async def get_all_members():
+    return {"message": "Hello members list"}
+
+
+# GET member_id
+@app.get("/members/{member_id}/")
+async def get_members(member_id: str):
+    return {"member_id": member_id}
+
+
+# GET member_id member_portfolio
+@app.get("/members/{member_id}/{portfolio_id}/")
+async def get_member_portolio(member_id: str, portfolio_id: str):
+    return {"member_id": member_id, "portfolio_id": portfolio_id}
+
+
+# POST add member_id
+@app.post("/add_member/{member_id}/")
+async def add_member(member_id: str):
+    return {"member_id": member_id}
+
+
+# PUT update member_id
+@app.put("/update_member/{member_id}/")
+async def update_member(member_id: str):
+    return {"member_id": member_id}
+
+
+# PUT remove member_id
+@app.put("/remove_member/{member_id}/")
+async def remove_member(member_id: str):
+    return {"member_id": member_id}
+
+
+if __name__ == "__main__":
+    uvicorn.run(app, timeout_keep_alive=65, host="0.0.0.0", port=5000)
